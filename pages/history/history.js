98d33c16646c2652d01a4a4868f337deba00e0a9
Notos <1051371520@qq.com>
2020-03-02 22:50:24 +0800

    库函数已导入

--- pages/history/history.js
+++ pages/history/history.js
@@ -2,6 +2,14 @@
 
 var base64 = require("../../images/base64");
 Page({
+  data: {
+    item: [
+      { first: '/images/icon_love.svg' },
+      { second: '/images/icon_love.svg'  },
+      { third: '/images/icon_love.svg' },
+    ]
+  },
+
 });
 
 
