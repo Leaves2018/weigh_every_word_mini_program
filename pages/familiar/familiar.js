35a5836ea94839a5ff406176a0079c242b2122b1
yuanyufei <yuanyufei1999@gmail.com>
2020-03-02 20:58:19 +0800

    添加iView Weapp组件；编写熟词本界面

--- pages/familiar/familiar.js
+++ pages/familiar/familiar.js
@@ -1,36 +1,51 @@
 var base64 = require("../../images/base64");
 Page({
   data: {
+    inputShowed: false,
+    inputVal: "",
     words: [{
-      name: "weigh",
-      chinese: "称量"
+      first: "weigh",
+      second: "every",
+      third: "word"
     }, {
-      name: "every",
-      chinese: "每一个"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }]
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }, {
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }, {
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }, {
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }, {
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }, {
+        first: "weigh",
+        second: "every",
+        third: "word"
+      }]
   },
   onLoad: function () {
     this.setData({
-      icon: base64.icon20,
-      slideButtons: [{
-        text: '普通',
-        src: '/images/icon_love.svg', // icon的路径
-      }, {
-        text: '普通',
-        extClass: 'test',
-        src: '/images/icon_star.svg', // icon的路径
-      }, {
-        type: 'warn',
-        text: '警示',
-        extClass: 'test',
-        src: '/images/icon_del.svg', // icon的路径
-      }],
+      search: this.search.bind(this)
     });
   },
-  slideButtonTap(e) {
-    console.log('slide button tap', e.detail)
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+      }, 200)
+    })
+  },
+  selectResult: function (e) {
+    console.log('select result', e.detail);
   }
 });
--- pages/vocabulary/vocabulary.js
+++ pages/vocabulary/vocabulary.js
@@ -1,66 +1,51 @@
-// pages/vocabulary/vocabulary.js
+var base64 = require("../../images/base64");
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    words: [{
+      name: "weigh",
+      chinese: "称量"
+    }, {
+      name: "every",
+      chinese: "每一个"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }, {
+      name: "word",
+      chinese: "单词"
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    this.setData({
+      icon: base64.icon20,
+      slideButtons: [{
+        text: '普通',
+        src: '/images/icon_love.svg', // icon的路径
+      }, {
+        text: '普通',
+        extClass: 'test',
+        src: '/images/icon_star.svg', // icon的路径
+      }, {
+        type: 'warn',
+        text: '警示',
+        extClass: 'test',
+        src: '/images/icon_del.svg', // icon的路径
+      }],
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  slideButtonTap(e) {
+    console.log('slide button tap', e.detail)
   }
-})
+});
