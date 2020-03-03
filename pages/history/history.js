5bacbf37045a0c521ba7ccd3948acf65659b8664
Notos <1051371520@qq.com>
2020-03-03 21:47:07 +0800

    完成历史界面
    删除不可用组件库，修改部分标题

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,14 +1,45 @@
 //search.js
-
 var base64 = require("../../images/base64");
 Page({
   data: {
-    item: [
-      { first: '/images/icon_love.svg' },
-      { second: '/images/icon_love.svg'  },
-      { third: '/images/icon_love.svg' },
-    ]
+    inputShowed: false,
+    inputVal: "",
+    items: [{
+      title: "Love is open.",
+    }, {
+      title: "mature love",
+    }, {
+      title: "Positive Meanings of Love",
+    }, {
+      title: "Love means having a responsibility",
+    }]
   },
+  onLoad: function () {
+    this.setData({
+      search: this.search.bind(this),
+      icon: base64.icon20,
+      slideButtons: [{
+        src: '/images/tabbar_icon_recite_active.png', // icon的路径
+      }, {
+        src: '/images/icon_star.svg', // icon的路径
+      }, {
+        src: '/images/icon_del.svg', // icon的路径
+      }],
+    });
+  },
+  slideButtonTap(e) {
+    console.log('slide button tap', e.detail)
+  },
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
+      }, 200)
+    })
+  },
+  selectResult: function (e) {
+    console.log('select result', e.detail);
+  }
 
 });
 
