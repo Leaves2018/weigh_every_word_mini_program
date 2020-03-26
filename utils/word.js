94adeeb4268a9bba6f5c0944dfd5ea2b7fc09984
Notos <1051371520@qq.com>
2020-03-26 11:58:24 +0800

    utils修改

--- utils/word.js
+++ utils/word.js
@@ -3,16 +3,34 @@ const db = wx.cloud.database();
 const dictionary = db.collection('dictionary');
 const _ = db.command;
 
-const getWordFromStorage = id => {
+const getWordFromStorage = async (id) => {
   var word = null;
+<<<<<<< HEAD
   word = wx.getStorageSync(id);
   if (typeof(word) === "string") {
     dictionary.doc(id).get().then(res => {
+=======
+  try {
+    word = wx.getStorageSync(id);
+    if (typeof(word) === "string") {
+      throw id + " is undefined in storage."
+    }
+    return word;
+  } catch (e) {
+    console.log(e);
+    await dictionary.doc(id).get().then(res => {
+>>>>>>> master
       word = res.data;
       wx.setStorage({
         key: id,
         data: word,
       })
+<<<<<<< HEAD
+=======
+    }).catch(reason => {
+      console.log(reason);
+      console.log("Fail to get word from cloud database dictionary.")
+>>>>>>> master
     })
   }
   return word;
