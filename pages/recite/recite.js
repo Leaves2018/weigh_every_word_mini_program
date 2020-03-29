d57e1fb4885588157806bce6171200cbea01dad5
yuanyufei <yuanyufei1999@gmail.com>
2020-03-29 17:29:15 +0800

    条件渲染配套逻辑代码
    DONE
    （1）实现条件渲染
    （2）设置hisotry_choice本地缓存key，在跳转到背诵页面前设置此key（值应为headline）即可命令背诵页面解析指定历史记录。

--- pages/recite/recite.js
+++ pages/recite/recite.js
@@ -5,8 +5,15 @@ const util_word = require('../../utils/word.js');
 
 Page({
   data: {
+    condition: true,
+    // 非背诵流程页面数据
+    msg_type: "",
+    msg_title: "",
+    msg_extend: [],
+    showReciteDoneDialog: false,
+    // 背诵流程页面数据
     progressOverall: 0,
-    word_tag: "Unknown",
+    word_tag: "",
     progressThis: 100,
     progressThisActive: false,
     thisDuration: 30,
@@ -17,6 +24,10 @@ Page({
     plain: true,
     loading: false
   },
+
+  /**
+   * 用于背诵流程的变量集合
+   */
   otherdata: {
     thisword: null,
     words: [],
@@ -24,28 +35,100 @@ Page({
     len: 0,
     vocabulary_words: [],
     unknown_words: [],
+    headline: "",
     history: null,
     new_unknown_words: [],
     new_vocabulary_words: [],
     new_familiar_words: [],
   },
-  onLoad: function () {
 
+  /**
+   * 清空背诵流程使用变量
+   */
+  clearOtherData: function () {
+    this.otherdata = {
+      thisword: null,
+      words: [],
+      cnt: -1,
+      len: 0,
+      vocabulary_words: [],
+      unknown_words: [],
+      headline: "",
+      history: null,
+      new_unknown_words: [],
+      new_vocabulary_words: [],
+      new_familiar_words: [],
+    };
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    this.initialRecitePage();
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
   onShow: function () {
-    new Promise(this.loadHistory).then(this.next).catch(function() {console.log("Something wrong.")});
+    this.generateHeadline();
   },
+
+  /**
+   * 生成标题
+   */
+  generateHeadline: function () {
+    try {
+      var history_choice = wx.getStorageSync('history_choice');
+      if (history_choice === "" || history_choice === -1) {
+        // TODO 给历史记录列表每个选项添加一个"done"属性，标志其是否已经背完
+        // TODO 并在没背完的部分中随机挑一个
+        // TODO 如果全部背完，修改reciteDone对话框内容并弹出
+        var history_list = util_his.getHistoryListFromStorage();
+        var len = history_list.length;
+        this.otherdata.headline = history_list[parseInt(Math.random() * len)];
+        // throw "history_choice is undefined in storage.";
+      } else if (typeof (history_choice) === "string") {
+        // TODO 安全性检查：确保history_choice就是history_list中的某个headline
+        this.otherdata.headline = history_choice;
+        this.goToRecite();
+      }
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      wx.setStorage({
+        key: 'history_choice',
+        data: -1,
+      })
+    }
+  },  
+
+   /**
+   * 初始化背诵页面
+   */
+  initialRecitePage: function () {
+    this.setData({
+      condition: true,
+      msg_type: "info",
+      msg_title: "要背单词吗？",
+      msg_extend: ["可以再来一组随机抽一组", "也可查看历史自己选择"],
+    });
+  },
+
+  /**
+   * 解析、加载历史记录
+   */
   loadHistory: function(resolve, reject) {
     console.log("In loadHistory,")
-    var headline = "";
-    if (arguments.length > 0 && typeof(arguments[0]) === "string") {
-      headline = arguments[0];
-    } else {
-      headline = util_his.getHistoryListFromStorage().pop();
-    }
-    console.log(headline);
-    this.otherdata.history = util_his.getHistoryFromStorage(headline);
-    
+    // var headline = "";
+    // if (arguments.length > 0 && typeof(arguments[0]) === "string") {
+    //   headline = arguments[0];
+    // } else {
+    //   headline = util_his.getHistoryListFromStorage().pop();
+    // }
+    // console.log(headline);
+    this.otherdata.history = util_his.getHistoryFromStorage(this.otherdata.headline);
     console.log(this.otherdata.history);
     this.otherdata.vocabulary_words = this.otherdata.history.vocabulary;
     this.otherdata.unknown_words = this.otherdata.history.unknown;
@@ -59,6 +142,10 @@ Page({
       reject();
     }
   },
+
+  /**
+   * 保存历史记录到本地缓存
+   */
   saveHistory: function() {
     this.otherdata.history.vocabulary = this.otherdata.new_vocabulary_words;
     this.otherdata.history.unknown = this.otherdata.new_unknown_words;
@@ -71,7 +158,13 @@ Page({
     util_word.appendVocabulary(nvw);
     util_word.deleteVocabulary(nvw);
     util_his.setHistoryInStorage(this.otherdata.history.headline, this.otherdata.history);
+    // 背诵完成，清空过程变量
+    this.clearOtherData();
   },
+
+  /**
+   * 背诵流程控制：切换页面数据到下一个单词
+   */
   next: function () {
     if (this.otherdata.unknown_words.length>0) {
       this.setData({
@@ -85,6 +178,7 @@ Page({
       })
     } else {
       this.reciteDone();
+      return;
     }
     util_word.getWord(this.otherdata.thisword.name).then(word => {
       // word.hesitateNum = 0;
@@ -100,6 +194,29 @@ Page({
       });
     });
   },
+
+  /**
+   * 没有单词背诵的处理方法
+   */
+  onNoWordsToRecite: function () {
+    console.log("No unknown words and vocabulary words to recite.");
+    this.setData({
+      showReciteDoneDialog: true,
+    });
+  },
+
+  /**
+   * 对话框提示没有单词背诵
+   */
+  tapReciteDoneDialogButton: function (e) {
+    this.setData({
+      showReciteDoneDialog: false,
+    });
+  },
+
+  /**
+   * 保存用户对单词的修改
+   */
   // saveWord: function () {
   //   let word = this.otherdata.thisword;
   //   wx.setStorage({
@@ -107,12 +224,40 @@ Page({
   //     data: word,
   //   })
   // },
+
+  /**
+   * 再次开始背诵流程
+   */
+  goToRecite: function () {
+    this.setData({
+      condition: false,
+    });
+    this.generateHeadline();
+    new Promise(this.loadHistory).then(this.next).catch(this.onNoWordsToRecite);
+  },
+
+  /**
+   * 跳转历史记录页面
+   */
+  goToHistory: function () {
+    wx.navigateTo({
+      url: '../history/history',
+    })
+  },
+
+  /**
+   * 背诵流程控制：激活“记得”“忘了”“模糊”按钮
+   */
   activateButtons: function () {
     this.setData({
       disabled: false,
       progressThisActive: false
     });
   },
+
+  /**
+   * 背诵流程控制：“模糊”按钮点击方法
+   */
   tapHesitate: function () {
     // thisword.hesitateNum += 1;
     this.setData({
@@ -120,24 +265,35 @@ Page({
       progressThisActive: true
     })
   },
+
+  /**
+   * 背诵流程控制：“忘了”按钮点击方法
+   */
   tapForget: function () {
     this.otherdata.new_vocabulary_words.push(this.otherdata.thisword);
     // this.saveWord();
     this.next();
   },
+
+  /**
+   * 背诵流程控制：“记得”按钮点击方法
+   */
   tapRemember: function () {
     this.otherdata.new_familiar_words.push(this.otherdata.thisword);
     // this.saveWord();
     this.next();
   },
+
+  /**
+   * 背诵完成控制方法
+   */
   reciteDone: function () {
-    console.log("recite done");
     this.saveHistory();
-    wx.navigateTo({
-      url: '../recite_done/recite_done',
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  }
+    this.setData({
+      condition: true,
+      msg_type: "success",
+      msg_title: "背诵完成",
+      msg_extend: ["Congratulations!"],
+    });
+  },
 });
