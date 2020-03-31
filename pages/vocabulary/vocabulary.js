a09f1ef6858c3128e3a7d71922991709f5cf7e02
yuanyufei <yuanyufei1999@gmail.com>
2020-03-31 12:48:41 +0800

    生词本页面数据由静态测试数据改为实际生词数据，并与word_detail页面关联

--- pages/vocabulary/vocabulary.js
+++ pages/vocabulary/vocabulary.js
@@ -1,38 +1,25 @@
 var base64 = require("../../images/base64")
+const util_trie = require('../../utils/trie.js');
+const util_word = require('../../utils/word.js');
 
 Page({
   data: {
-    inputShowed: false,
+    showWordDetail: false,
     inputVal: "",
-    words: [{
-      name: "weigh",
-      chinese: "称量"
-    }, {
-      name: "every",
-      chinese: "每一个"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }, {
-      name: "word",
-      chinese: "单词"
-    }]
+    vocabulary_words: [],
+  },
+  otherdata: {
+    vocabulary_trie: null,
+    vocabulary_words: [],
   },
   onLoad: function () {
+    this.otherdata.vocabulary_trie = util_word.getVocabulary();
+    this.otherdata.vocabulary_words = this.otherdata.vocabulary_trie.getAllData();
+    // console.log("In onLoad of vocabulary.js,");
+    // console.log(this.otherdata.vocabulary_words);
     this.setData({
       search: this.search.bind(this),
+      vocabulary_words: this.otherdata.vocabulary_words,
       icon: base64.icon20,
       slideButtons: [{
         text: '普通',
@@ -49,8 +36,34 @@ Page({
       }],
     });
   },
-  slideButtonTap(e) {
-    console.log('slide button tap', e.detail)
+  tapSlideView(e) {
+    // console.log('slide view ', e.detail);
+    let index = e.currentTarget.dataset.position;
+    // let _id = this.otherdata.vocabulary_words[index];
+    // util_word.getWord(_id).then(word => {
+    //   this.setData({
+    //     showWordDetail: true,
+    //     word_level: word.level,
+    //     word_id: word._id,
+    //     word_phonetic: word.phonetic,
+    //     word_chinese: word.chinese,
+    //     word_context: word.context,
+    //     word_english: word.english,
+    //   });
+    // });
+
+    wx.setStorage({
+      key: 'word_detail_list',
+      data: {
+        trie: this.otherdata.vocabulary_trie,
+        words: this.otherdata.vocabulary_words,
+        currentIndex: index,
+      },
+    });
+
+    wx.navigateTo({
+      url: '../word_detail/word_detail',
+    });
   },
   search: function (value) {
     return new Promise((resolve, reject) => {
