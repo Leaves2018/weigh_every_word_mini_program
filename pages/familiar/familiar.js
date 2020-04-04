1af744d0e6fb66df70726b20e8929f6b682a629e
yuanyufei <yuanyufei1999@gmail.com>
2020-04-04 11:35:51 +0800

    熟词本页面完善
    * 完成熟词本的单词详情对话框中“忘了”方法的点击事件（即保存修改记录）
    * 熟词本页面按照字母进行分类显示

--- pages/familiar/familiar.js
+++ pages/familiar/familiar.js
@@ -1,7 +1,12 @@
 const utilWord = require('../../utils/word.js');
+const utilTrie = require('../../utils/trie.js');
 var base64 = require("../../images/base64");
 var familiarTrie = null;
 var familiarWords = [];
+var currentInitialIndex = 0;
+var currentWordIndex = 0;
+var vocabularyWords = [];
+var initials = [];
 Page({
   data: { 
     inputShowed: false,
@@ -13,14 +18,31 @@ Page({
   },
   onLoad: function () {
     familiarTrie = utilWord.getFamiliar();
-    familiarWords = familiarTrie.getAllData();
+    // familiarWords = familiarTrie.getAllData();
+    // 每次取树的一个子节点创建新树，利用新树的getAllData()方法获取首字母相同的单词数组
+    // 考虑封装到Trie类结构中，或者修改Trie的定义
+    for (let child of familiarTrie.root.children) {
+      initials.push(child.key.toUpperCase());
+      var tempTrie = new utilTrie.Trie();
+      tempTrie.root.children[0] = child;
+      familiarWords.push(tempTrie.getAllData());
+    }
     this.setData({
       search: this.search.bind(this),
+      initials: initials,
       words: familiarWords,
     });
   },
   onUnload: function () {
-
+    utilWord.setFamiliar(familiarTrie);
+    utilWord.appendVocabulary(vocabularyWords);
+    // 手动清空
+    familiarTrie = null;
+    familiarWords = [];
+    currentInitialIndex = 0;
+    currentWordIndex = 0;
+    vocabularyWords = [];
+    initials = [];
   },
   search: function (value) {
     return new Promise((resolve, reject) => {
@@ -33,9 +55,8 @@ Page({
     console.log('select result', e.detail);
   },
   showDetail: function (e) {
-    let index;
-    index = e.currentTarget.dataset.position;
-    let _id = this.data.words[index];
+    [currentInitialIndex, currentWordIndex] = e.currentTarget.dataset.position.split('.');
+    let _id = this.data.words[currentInitialIndex][currentWordIndex];
     utilWord.getWord(_id).then(word => {
       this.setData({
         dialogTitle: word._id,
@@ -44,11 +65,22 @@ Page({
       })
     });
   },
-  tapDialogButton(e) {
+  tapDialogButton: function (e) {
+    switch (e.detail.index) {
+      case 0:
+        let _id = familiarWords[currentInitialIndex][currentWordIndex];
+        familiarWords[currentInitialIndex].splice(currentWordIndex, 1);
+        familiarTrie.deleteData(_id);
+        vocabularyWords.push(_id);
+        break;
+      case 1:
+        break;
+    }
     this.setData({
-      dialogShow: false
+      dialogShow: false,
+      words: familiarWords,
     });
-  }
+  },
 });
 
 
