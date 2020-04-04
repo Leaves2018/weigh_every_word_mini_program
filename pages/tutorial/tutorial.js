fbccebf2ca0bd4452154d5fa12aca1e9b6f0ccfa
yuanyufei <yuanyufei1999@gmail.com>
2020-04-04 22:37:58 +0800

    测试动画

--- pages/tutorial/tutorial.js
+++ pages/tutorial/tutorial.js
@@ -25,9 +25,9 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  // onShow: function () {
 
-  },
+  // },
 
   /**
    * 生命周期函数--监听页面隐藏
@@ -62,5 +62,106 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  onShow: function () {
+
+    // animation.scale(2, 2).rotate(45).step()
+
+    // this.setData({
+    //   animationData: animation.export()
+    // })
+
+    // setTimeout(function () {
+    //   animation.translate(30).step()
+    //   this.setData({
+    //     animationData: animation.export()
+    //   })
+    // }.bind(this), 1000)
+  },
+  rotateAndScale: function () {
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    // 旋转同时放大
+    this.animation.rotate(45).scale(2, 2).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  rotateThenScale: function () {
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    // 先旋转后放大
+    this.animation.rotate(45).step()
+    this.animation.scale(2, 2).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  rotateThenScaleBack: function () {
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    // 先旋转后放大-回退
+    this.animation.scale(1, 1).step()
+    this.animation.rotate(0).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  rotateAndScaleThenTranslate: function () {
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    // 先旋转同时放大，然后平移
+    this.animation.rotate(45).scale(2, 2).step()
+    this.animation.translate(100, 100).step({ duration: 1000 })
+    this.setData({
+      animationData: this.animation,
+    })
+  },
+
+  translateLeft: function () {
+    var animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'ease',
+    })
+
+    this.animation = animation
+    // 先旋转后放大
+    this.animation.translateX(-500).step();
+    this.animation.translateY(-500).step();
+    this.animation.translateX(0).step();
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  translateBack: function () {
+    // var animation = wx.createAnimation({
+    //   duration: 1000,
+    //   timingFunction: 'ease',
+    // })
+
+    // this.animation = animation
+    // 先旋转后放大-回退
+    // this.animation.scale(1, 1).step()
+    this.animation.translateY(0).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
 })
