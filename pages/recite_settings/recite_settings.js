b45f51790c7c4a26aec694fcca1f032ae9188840
Notos <1051371520@qq.com>
2020-04-07 16:08:26 +0800

    暂时在背诵选项添加了清空缓存按钮

--- pages/recite_settings/recite_settings.js
+++ pages/recite_settings/recite_settings.js
@@ -3,9 +3,31 @@ const app = getApp()
 
 Page({
   data: {
+    mHidden: true,
   },
 
   sliderchange: function (e) {
     console.log(e.detail.value);
+  },
+  clear_storage: function () {
+    this.setData({
+      mHidden: false,
+    });
+  },
+  modalconfirm: function () {
+    this.setData({
+      mHidden: true,
+    });
+    try {
+      wx.clearStorageSync()
+    } catch (e) {
+      // Do something when catch error
+    }
+  },
+
+  modalcancel: function () {
+    this.setData({
+      mHidden: true,
+    });
   },
 })
