7ff894674d2782bada2e88d16910fd1c1a670094
yuanyufei <yuanyufei1999@gmail.com>
2020-04-25 22:28:28 +0800

    实现了更精确的字符匹配与标记

--- utils/tomd.js
+++ utils/tomd.js
@@ -1,7 +1,8 @@
 // 在指定文本text中用指定标记markup包裹指定内容content
 const markText = (text, content, markup="**", g=true) => {
   var res = [];
-  var re = new RegExp(content, 'i' + (g ? 'g' : ''));
+  var re = new RegExp(`[^a-zA-Z]+${content}[^a-zA-Z]+`, 'i' + (g ? 'g' : ''));
+  var re2 = new RegExp(content, 'i' + (g ? 'g' : ''));
   var lastIndex = 0;
 
   while (true) {
@@ -11,8 +12,17 @@ const markText = (text, content, markup="**", g=true) => {
       res.push(text.substring(lastIndex));
       break;
     }
-    res.push(text.substring(lastIndex, temp.index));
-    res.push(`${markup}${text.substring(temp.index, re.lastIndex)}${markup}`);
+    res.push(text.substring(lastIndex, temp.index));  // 插入上一次匹配成功的结束位置到此次匹配的开始位置的子串
+
+    let temptext = text.substring(temp.index, re.lastIndex);  // 取正则表达式匹配结果字符串
+    let idx1 = temptext.toLowerCase().indexOf(content.toLowerCase()); // 在匹配结果字符串里再精确查找content的（开始）位置
+    let idx2 = idx1 + content.length; // 计算结束位置
+    // 分前、中、后三部分，“中”部分即为需要标记内容，做MD标记后push
+    res.push(temptext.substring(0, idx1));
+    res.push(`${markup}${temptext.substring(idx1, idx2)}${markup}`);
+    res.push(temptext.substring(idx2));
+
+    // res.push(`${markup}${text.substring(temp.index, re.lastIndex)}${markup}`);
   }
   return res.join('');
 }
