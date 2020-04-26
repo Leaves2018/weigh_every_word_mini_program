00222d5a3df203c4c61e8f66e50b02bf4f2863d9
Notos <1051371520@qq.com>
2020-04-26 22:46:57 +0800

    设置界面进度条解决

--- pages/recite_settings/recite_settings.js
+++ pages/recite_settings/recite_settings.js
@@ -5,13 +5,32 @@ const utils_lc = require('../../utils/local_cloud.js');
 Page({
   data: {
     mHidden: true,
+    isDown: false,
+    percent: 0,
+    nHidden: true,
   },
   local_to_cloud: function () {
+    this.setData({
+      nHidden: false,
+      isDown: true,
+      percent: 99,
+    });
     utils_lc.local_to_cloud();
+    this.setData({
+      percent: 100,
+    });
   },
 
   cloud_to_local: function () {
+    this.setData({
+      nHidden: false,
+      isDown: true,
+      percent: 99,
+    });
     utils_lc.cloud_to_local();
+    this.setData({
+      percent: 100,
+    });
   },
 
   sliderchange: function (e) {
@@ -36,6 +55,11 @@ Page({
   modalcancel: function () {
     this.setData({
       mHidden: true,
+    });
+  },
+  modalconfirm1: function () {
+    this.setData({
+      nHidden: true
     });
   },
 })
