3d806fea343c4ae1be23173ab8542eda4bf2fa88
yuanyufei <yuanyufei1999@gmail.com>
2020-04-29 11:53:54 +0800

    新增 markTextWithExchange方法，支持同时标记单词与其变形；取消markText的g参数（其如果为false似乎将导致死循环）

--- utils/tomd.js
+++ utils/tomd.js
@@ -1,8 +1,9 @@
+const utilWord = require('./word2.js');
 // 在指定文本text中用指定标记markup包裹指定内容content
-const markText = (text, content, markup="**", g=true) => {
+const markText = (text, content, markup="**") => {
   var res = [];
-  var re = new RegExp(`[^a-zA-Z]+${content}[^a-zA-Z]+`, 'i' + (g ? 'g' : ''));
-  var re2 = new RegExp(content, 'i' + (g ? 'g' : ''));
+  text = ` ${text} `; // 前后各加一个非字母字符，已解决开头结尾无法标记的问题
+  var re = new RegExp(`[^a-zA-Z]${content}[^a-zA-Z]`, 'ig');
   var lastIndex = 0;
 
   while (true) {
@@ -15,14 +16,17 @@ const markText = (text, content, markup="**", g=true) => {
     res.push(text.substring(lastIndex, temp.index));  // 插入上一次匹配成功的结束位置到此次匹配的开始位置的子串
 
     let temptext = text.substring(temp.index, re.lastIndex);  // 取正则表达式匹配结果字符串
-    let idx1 = temptext.toLowerCase().indexOf(content.toLowerCase()); // 在匹配结果字符串里再精确查找content的（开始）位置
-    let idx2 = idx1 + content.length; // 计算结束位置
-    // 分前、中、后三部分，“中”部分即为需要标记内容，做MD标记后push
-    res.push(temptext.substring(0, idx1));
-    res.push(`${markup}${temptext.substring(idx1, idx2)}${markup}`);
-    res.push(temptext.substring(idx2));
-
-    // res.push(`${markup}${text.substring(temp.index, re.lastIndex)}${markup}`);
+    // 如果超长，则证明多匹配了
+    // if (temptext.length > content.length) {
+      let idx1 = temptext.toLowerCase().indexOf(content.toLowerCase()); // 在匹配结果字符串里再精确查找content的（开始）位置
+      let idx2 = idx1 + content.length; // 计算结束位置
+      // 分前、中、后三部分，“中”部分即为需要标记内容，做MD标记后push
+      res.push(temptext.substring(0, idx1));
+      res.push(`${markup}${temptext.substring(idx1, idx2)}${markup}`);
+      res.push(temptext.substring(idx2));
+    // } else {
+    //   res.push(`${markup}${temptext}${markup}`);
+    // }
   }
   return res.join('');
 }
@@ -35,8 +39,13 @@ const markArticle = (article, contents, markup="**") => {
   return article;
 }
 
+// 在指定文本text中用指定标记markup包裹指定单词word及其变形
+const markTextWithExchange = (text, word, markup="**") => {
+  return markArticle(text, Object.values((new utilWord.Word(word)).getExchange()).concat([word._id]), markup);
+}
+
 module.exports = {
   markText: markText,
   markArticle: markArticle,
+  markTextWithExchange: markTextWithExchange,
 }
-
