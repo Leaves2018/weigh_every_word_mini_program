d576419a7b632983ebe0586b583fb7085d150117
yuanyufei <yuanyufei1999@gmail.com>
2020-05-03 09:15:54 +0800

    Loading组件测试

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,39 +1,17 @@
 Page({
   data: {
-    objectArray: [
-      { id: 5, unique: 'unique_5' },
-      { id: 4, unique: 'unique_4' },
-      { id: 3, unique: 'unique_3' },
-      { id: 2, unique: 'unique_2' },
-      { id: 1, unique: 'unique_1' },
-      { id: 0, unique: 'unique_0' },
-    ],
-    numberArray: [1, 2, 3, 4]
+    tips: '请稍后',
+    show: true,
+    animated: true
   },
-  switch: function (e) {
-    const length = this.data.objectArray.length
-    for (let i = 0; i < length; ++i) {
-      const x = Math.floor(Math.random() * length)
-      const y = Math.floor(Math.random() * length)
-      const temp = this.data.objectArray[x]
-      this.data.objectArray[x] = this.data.objectArray[y]
-      this.data.objectArray[y] = temp
-    }
-    this.setData({
-      objectArray: this.data.objectArray
-    })
+  onShow() {
+    this.timer = setInterval(() => {
+      this.setData({
+        show: !this.data.show
+      })
+    }, 2000)
   },
-  addToFront: function (e) {
-    const length = this.data.objectArray.length
-    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
-    this.setData({
-      objectArray: this.data.objectArray
-    })
-  },
-  addNumberToFront: function (e) {
-    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
-    this.setData({
-      numberArray: this.data.numberArray
-    })
+  onUnload() {
+    clearInterval(this.timer)
   }
 })
