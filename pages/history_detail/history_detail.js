615b76074205a661e4aefee18abfb833c22bfc21
Notos <1051371520@qq.com>
2020-05-03 09:35:38 +0800

    文章详情2使用component改写

--- pages/history_detail/history_detail.js
+++ pages/history_detail/history_detail.js
@@ -1,11 +1,11 @@
 //search.js
 const utils_his = require('../../utils/history.js');
 const utils_tomd = require('../../utils/tomd.js');
-var base64 = require("../../images/base64");
 const util_word = require('../../utils/word2.js');
 const util_trie = require('../../utils/trie.js');
 const utils_deal = require('../../utils/deal.js');
 const app = getApp();
+
 function history(headline, body, vocabulary, unknown, date) {
   this.headline = headline;
   this.body = body;
@@ -20,20 +20,23 @@ var unknown_remember_vocabulary = [];
 var voc_remember_vocabulary = [];
 var unknown = [];
 var history_example;
-var before_headline;
 Page({
   data: {
-    his_headline:"",
-    his_body:[],
-    his_vocabulary:[],
-    his_unknown:[],
-    his_date:"",
+    his_headline: "",
+    his_body: [],
+    his_vocabulary: [],
+    his_unknown: [],
+    his_date: "",
     dialogShow: false,
     dialogTitle: "",
     dialogContent: "",
-    buttons: [{ text: "忘了" }, { text: "记得" }],
+    buttons: [{
+      text: "忘了"
+    }, {
+      text: "记得"
+    }],
   },
-  onLoad: function () {
+  onLoad: function() {
     var history_detail = wx.getStorageSync('history_detail');
     let history_example0 = utils_his.getHistoryFromStorage(history_detail);
     let headlineTemp = utils_deal.deal_passageWithHeadline(history_example0.headline, history_example0.body.join(' '));
@@ -42,15 +45,18 @@ Page({
     unknown = [];
     vocabulary = history_example.vocabulary.map(element => element.name);
     unknown = history_example.unknown.map(element => element.name);
-    before_headline = history_example.headline;
     let article_mes = history_example.body.join(' ');
     let article_temp0 = article_mes.toLowerCase();
     let article_temp = article_temp0.replace(/[^a-zA-Z]/g, ' ');
     let article_words = article_temp.split(" "); //获取单词
-    article_words = [...new Set(article_words)];//单词去重
-    article_words = article_words.filter(function (x) { return x && x.trim(); });//单词去空
-    article_words = article_words.filter(function (x) { return x.length > 2; });
-    var his_body_temp0 = utils_tomd.markArticle(article_mes, article_words, '*');//TODO等待雨飞的函数
+    article_words = [...new Set(article_words)]; //单词去重
+    article_words = article_words.filter(function(x) {
+      return x && x.trim();
+    }); //单词去空
+    article_words = article_words.filter(function(x) {
+      return x.length > 2;
+    });
+    var his_body_temp0 = utils_tomd.markArticle(article_mes, article_words, '*'); //TODO等待雨飞的函数
     var his_body_temp = utils_tomd.markArticle(his_body_temp0, vocabulary, '==');
     var his_body_res = utils_tomd.markArticle(his_body_temp, unknown, '++');
     var his_body_result = app.towxml(his_body_res, 'markdown', {
@@ -80,28 +86,18 @@ Page({
   //将修改存至本地
   onUnload: function() {
     wx.removeStorage({
-      key: before_headline,
-      success: function (res) {
+      key: history_example.headline,
+      success: function(res) {
         //console.log(res);
       },
     })
-    var history_list = utils_his.getHistoryListFromStorage();
-    var temp;
-    for (var i = 0; i < history_list.length; i++) {
-      if (history_list[i] == before_headline) {
-        temp = i;
-      }
-    }
-    history_list.splice(temp, 1, history_example.headline);
 
     if (history_example.vocabulary.length === 0 && history_example.unknown.length === 0) {
       let history_done_list = utils_his.getHistoryListDoneFromStorage();
       history_done_list.splice(temp, 1, 'success');
       utils_his.setHistoryListDoneInStorage(history_done_list);
     }
-
-    //console.log(history_list);
-    utils_his.setHistoryListInStorage(history_list);
+    
     var history_result = new history(history_example.headline, history_example.body, history_example.vocabulary, history_example.unknown, history_example.date);
     utils_his.setHistoryInStorage(history_example.headline, history_result);
     util_word.appendVocabulary(vocabulary);
@@ -109,36 +105,36 @@ Page({
     util_word.appendFamiliar(unknown_remember_vocabulary);
   },
 
-  article_modify: function () {
+  article_modify: function() {
     wx.redirectTo({
       url: '/pages/history_detail_modify/history_detail_modify',
     });
   },
 
   //背诵
-  recite: function () {
+  recite: function() {
     wx.setStorage({
       key: 'recite_info',
       data: {
         type: 'history',
         headline: history_example.headline,
       },
-      success: function () {
+      success: function() {
         wx.redirectTo({
           url: '/pages/recite/recite',
         });
       }
     })
   },
-  
-  tapDialogButton: function (e) {
+
+  tapDialogButton: function(e) {
     if (word_tag === "mark") {
       var temp_index = vocabulary.indexOf(deal_word);
       switch (e.detail.index) {
         //忘了
         case 0:
           break;
-        //记得
+          //记得
         case 1:
           voc_remember_vocabulary.push(deal_word);
           unknown_remember_vocabulary.push(vocabulary[temp_index]);
@@ -158,7 +154,7 @@ Page({
           history_example.unknown.splice(temp_index, 1);
           history_example.vocabulary.push(addword);
           break;
-        //记得
+          //记得
         case 1:
           unknown_remember_vocabulary.push(unknown[temp_index]);
           unknown.splice(temp_index, 1);
@@ -174,7 +170,7 @@ Page({
           vocabulary.push(deal_word);
           history_example.vocabulary.push(deal_word);
           break;
-        //记得
+          //记得
         case 1:
           break;
       }
@@ -185,12 +181,8 @@ Page({
       his_unknown: unknown,
     });
   },
-  
+
   // buttontap(e) {
   //   console.log(e.detail)
   // }
 });
-
-
-
-
