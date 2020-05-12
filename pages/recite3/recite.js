36556399740035901bb86c4b5a032b1c6536bd94
yuanyufei <yuanyufei1999@gmail.com>
2020-05-12 16:50:58 +0800

    修复 顶部底部空余区域+加载缓慢
    * 重新设置recycle-view高度以及container高度以消除空余区域
    * 使用ctx的append方法而不是setData解决加载缓慢问题
    * 高度设置无效的原因是，recycleView的height属性应该传递一个Number，默认为“px”（不需要手动加）
    * wx:if套在recycleView时会出错，可以通过动态设置高度实现

--- pages/recite3/recite.js
+++ pages/recite3/recite.js
@@ -34,13 +34,7 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {
-    buttons: [{
-      text: "忘了"
-    }, {
-      text: "记得"
-    }],
-  },
+  data: {},
 
   lifetimes: {
     /**
@@ -49,23 +43,29 @@ Component({
     attached: function() {
       console.log("recite component is attached")
 
-      let wordCardWidth = app.globalData.windowWidth * 0.8;
-      let wordCardHeight = app.globalData.windowHeight * 0.618;
+    },
+    /**
+     * 视图层渲染完成
+     */
+    ready: function() {
+      console.log("recite component is ready")
+      let systemInfo = wx.getSystemInfoSync();
+      console.log(JSON.stringify(systemInfo))
+      let wordCardWidth = Math.round(systemInfo.windowWidth * 0.8);
+      let wordCardHeight = Math.round(systemInfo.windowHeight * 0.618);
       this.setData({
         wordCardWidth: wordCardWidth + "px",
         wordCardHeight: wordCardHeight + "px",
-        recycleViewHeight: app.globalData.windowHeight,
-        recycleViewWidth: app.globalData.windowWidth * 0.8,
+        recycleViewHeight: systemInfo.windowHeight,
+        recycleViewWidth: systemInfo.windowWidth * 0.8,
       })
-      var ctx = createRecycleContext({
-        id: 'wordCardRrecycleId',
-        dataKey: 'wordCardList',
-        page: this,
-        itemSize: {
-          width: wordCardWidth,
-          height: wordCardHeight + 25,
-        }
-      })
+    },
+    /**
+     * 组件卸载
+     */
+    detached: function() {
+      console.log("recite component is detached");
+      if (this.ctx) this.ctx.destroy();
     }
   },
   /**
@@ -73,14 +73,14 @@ Component({
    */
   methods: {
     close: function close() {
-      console.log("In recite3, close() is called.")
+      console.log("In recite3, close() is called.");
       if (!this.data.maskClosable) return;
       this.setData({
         show: false
       });
       this.triggerEvent('close', {}, {});
     },
-    stopEvent: function stopEvent() { },
+    stopEvent: function stopEvent() {},
     buttonTap: function(e) {
       console.log(e);
       this.triggerEvent("buttontap", e.detail, {}); // 继续触发事件，向上冒泡
@@ -93,6 +93,7 @@ Component({
     'show': function(show) {
       console.log('show=' + JSON.stringify(show))
       if (show) {
+        // 每次显示时，重新读取历史记录并解析
         let history = this.data.history;
         if (history) {
           var wordCardList = [];
@@ -106,9 +107,18 @@ Component({
               })
             }
           }
-          this.setData({
-            wordCardList: wordCardList,
+          // 销毁原有的ctx，重新建立ctx并赋予新数据
+          if (this.ctx) this.ctx.destroy();
+          this.ctx = createRecycleContext({
+            id: 'wordCardRrecycleId',
+            dataKey: 'wordCardList',
+            page: this,
+            itemSize: {
+              width: parseInt(this.data.wordCardWidth),
+              height: parseInt(this.data.wordCardHeight) + 25,
+            }
           })
+          this.ctx.append(wordCardList)
         }
       }
     }
