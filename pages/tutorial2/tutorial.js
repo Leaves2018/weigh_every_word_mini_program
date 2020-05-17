e186ca4e91f398bed6164db8500b711da70d862e
yuanyufei <yuanyufei1999@gmail.com>
2020-05-17 12:20:39 +0800

    试验halfScreenDialog

--- pages/tutorial2/tutorial.js
+++ pages/tutorial2/tutorial.js
@@ -1,9 +1,27 @@
-const order = ['demo1', 'demo2', 'demo3']
-
 Page({
-
   data: {
-
+    show: false,
+    buttons: [
+      {
+        type: 'default',
+        className: '',
+        text: '忘记',
+        value: 0
+      },
+      {
+        type: 'primary',
+        className: '',
+        text: '记得',
+        value: 1
+      }
+    ]
   },
-  
-})
+  open: function () {
+    this.setData({
+      show: true
+    })
+  },
+  buttontap(e) {
+    console.log(e)
+  }
+});
