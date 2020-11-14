f577c245d5da3f05897970b0658f9dfe1105d12f
yuanyufei <yuanyufei1999@gmail.com>
2020-11-15 07:37:51 +0800

    解决冲突

--- functions/mysqlDatabase/index.js
+++ functions/mysqlDatabase/index.js
@@ -16,8 +16,10 @@ exports.main = async (event, context) => {
     const conn = await mysql.createConnection({
       host: "rm-bp1z1q66g7pyyc32klo.mysql.rds.aliyuncs.com",
       database: "weigh_every_word",
-      user: "wechat_mp",
-      password: "ljC07GkR2s0)0nY!t(hyFO8Lwk46e_Dz"
+      // user: "wechat_mp",
+      // password: "ljC07GkR2s0)0nY!t(hyFO8Lwk46e_Dz"
+      user: "ryan",
+      password: "Leaves9091"
     });
     // 注入event对象，供后续处理函数调用
     event['conn'] = conn;
@@ -32,15 +34,15 @@ exports.main = async (event, context) => {
     case 'execute': {
       return execute(event)
     }
+    // 添加用户信息，如登记用户学号、邮箱和OPENID
     case 'addUser': {
       return addUser(event)
     }
-    case 'updateUser': {
-      return updateUser(event)
-    }
+    // 向数据库插入文本
     case 'addText': {
       return addText(event)
     }
+    // 查询语料库
     case 'searchCorpusByWord': {
       return searchCorpusByWord(event)
     }
@@ -100,63 +102,37 @@ async function addUser(event) {
   }
 }
 
-// 更新用户账户信息
-async function updateUser(event) {
-  const { OPENID } = cloud.getWXContext();  // 获取用户openid
-  const { conn, userData } = event;
-  try {
-    const userTableColumnNames = ['nickname', 'email', 'rules', 'searchMe', 'emailSwitch'];
-    let keys = Object.keys(userData).filter(x => userTableColumnNames.indexOf(x) >= 0);  // 只有指定的数据可以被更新
-    let values = keys.map(x => conn.escape(userData[x])); // 获取需要更新的值，并转义
-    // 组装SQL的SET子句
-    let setClauseArray = [];
-    for (let i = 0, len = keys.length; i < len; i++) {
-      setClauseArray.push(`${keys[i]}=${values[i]}`)
-    }
-    // 完整拼装SQL语句
-    var sql = `UPDATE user SET ${setClauseArray.join(',')} WHERE openid=${conn.escape(OPENID)}`;
-    console.log(sql);
-    const res = await conn.execute(sql);
-    await conn.end();
-    return { res, sql };
-  } catch (err) {
-    console.error(err);
-    await conn.end();
-    return { err, sql }
-  }
-}
-
 async function addText(event) {
-  const { OPENID } = cloud.getWXContext();  // 获取用户openid
-  const { conn, title, body } = event;
+  // 获取用户OPENID
+  const { OPENID } = cloud.getWXContext();
+  var { conn, textData} = event;
   try {
-    var sql = `INSERT INTO text (_openid, title, body) VALUES(${conn.escape(OPENID)}, ${conn.escape(title)}, ${conn.escape(body)});`;
-    console.log('In addText(): ' + sql);
+    var sql = `INSERT INTO text (_openid, title, body) VALUES(${conn.escape(OPENID)}, ${conn.escape(textData.title)}, ${conn.escape(textData.body)});`;
+    console.log("In addText(): " + sql);
     var result = await conn.execute(sql);
     conn.end();
-    return { data: result, sql };
+    return {data: result, sql};
   } catch (err) {
-    console.error('添加文本失败')
+    console.error("添加文本记录失败")
     var result = err;
     conn.end();
-    return { err, sql };
+    return {err, sql};
   }
 }
 
 async function searchCorpusByWord(event) {
-  const { OPENID } = cloud.getWXContext();  // 获取用户openid
-  const { conn, word } = event;
-  wordPattern = `%${word}%`
+  const {OPENID} = cloud.getWXContext();
+  var { conn, word } = event;
   try {
-    var sql = `SELECT * FROM text WHERE _openid=${conn.escape(OPENID)} AND body LIKE ${conn.escape(wordPattern)}`;
-    console.log('In searchCorpusByWord(): ' + sql);
+    var sql = `SELECT * FROM text WHERE _openid=${conn.escape(OPENID)} and ${conn.escape(word)} IN body;`
+    console.log("In searchCorpusByWord(): " + sql);
     var result = await conn.execute(sql);
     conn.end();
-    return { data: result, sql };
+    return {data: result, sql};
   } catch (err) {
-    console.error('查询语料库失败')
+    console.error("查询语料库失败")
     var result = err;
     conn.end();
-    return { err, sql };
+    return {err, sql};
   }
 }
