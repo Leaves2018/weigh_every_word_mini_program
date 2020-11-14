491b4fb381d82ff187c3954318d06620c62dbf2a
Notos <1051371520@qq.com>
2020-11-14 23:39:21 +0800

    添加开发测试页面test

---
+++ pages/test/test.js
@@ -0,0 +1,54 @@
+// pages/test/test.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    connectDatabase(e) {
+      wx.cloud.callFunction({
+        name: 'mysqlDatabase',
+        data: {
+          action: 'addText',
+          title: 'My name is Ritian Zhao.',
+          body: 'My duty is to make a new world.'
+        },
+        success(res) {
+          console.log(JSON.stringify(res))
+        },
+        fail(err) {
+          console.error(JSON.stringify(err))
+        }
+      })
+    },
+    searchDatabase(e) {
+      wx.cloud.callFunction({
+        name: 'mysqlDatabase',
+        data: {
+          action: 'searchCorpusByWord',
+          word: 'test'
+        },
+        success(res) {
+          console.log(JSON.stringify(res.result.data[0]))
+        },
+        fail(err) {
+          console.error(JSON.stringify(err))
+        }
+      })
+    }
+
+  }
+})
