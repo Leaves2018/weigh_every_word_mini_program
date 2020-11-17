1a4c5ebb76e3371a9b4719db7080f3b3a89c26e2
yuanyufei <yuanyufei1999@gmail.com>
2020-11-17 22:39:37 +0800

    添加无语料库查询结果时的提示

--- components/corpus-list/corpus-list.js
+++ components/corpus-list/corpus-list.js
@@ -22,6 +22,7 @@ Component({
    * 组件的初始数据
    */
   data: {
+    haveCorpusData: false,
     icon: base64.icon20,
     useIcon: true,
     slideButtons: [{
@@ -77,16 +78,23 @@ Component({
           // 搜索成功，将返回内容显示到视图中
           success(res) {
             let corpusList = res.result.data[0]
-            let corpusTimeList = corpusList.map(x => new Date(x.time).toLocaleString());
-            console.log(corpusList)
-            // 抽取每条搜索结果的正文内容中包括目标单词word的句子，并用**标记（表示加粗）
-            let sentences = corpusList.map(x => util.findTheSentenceWhereTheWordIs(x.body, word));
-            let markdownSentences = sentences.map(x => tomd.markText(x, word, '**'));
-            that.setData({
-              corpusSentenceList: markdownSentences.map(x => app.towxml(`*${x}*`, 'markdown')),
-              corpusList,
-              corpusTimeList,
-            })
+            if (corpusList.length > 0) {
+              let corpusTimeList = corpusList.map(x => new Date(x.time).toLocaleString());
+              console.log(corpusList)
+              // 抽取每条搜索结果的正文内容中包括目标单词word的句子，并用**标记（表示加粗）
+              let sentences = corpusList.map(x => util.findTheSentenceWhereTheWordIs(x.body, word));
+              let markdownSentences = sentences.map(x => tomd.markText(x, word, '**'));
+              that.setData({
+                haveCorpusData: true,
+                corpusSentenceList: markdownSentences.map(x => app.towxml(`*${x}*`, 'markdown')),
+                corpusList,
+                corpusTimeList,
+              }) 
+            } else {
+              that.setData({
+                haveCorpusData: false
+              })
+            }
           },
           fail(err) {
             console.error(JSON.stringify(err))
